f7f75a3a9678120eb1c33a04724d0db7edf4250f
tomonakar <tomohisa.nak@gmail.com>
2020-12-08 17:34:39 +0900

    feat: 引数を表示するよう更新

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func Hello(name string) string {
-	return "Hello, world"
+	return "Hello, " + name
 }
+
 func main() {
-	// fmt.Println("Hello, world!")
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("Chris"))
 }
