36ce2e9fc9a70e0530befcd54f139ebd3ba7a374
tomonakar <tomohisa.nak@gmail.com>
2020-12-11 08:38:59 +0900

    fix: SumAll

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,8 +9,7 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+	var sums []int
 
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
