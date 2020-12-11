ed64bcf969ae6368ee001457e2fc0821d3bb06e4
tomonakar <tomohisa.nak@gmail.com>
2020-12-11 09:22:15 +0900

    テストを作成 関数を作成

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,5 +1,14 @@
 package structs
 
-func Perimeter(width, height float64) float64 {
-	return 0
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
