8ea6ac1def95989cc7ebe24838f69cc82417433b
tomonakar <tomohisa.nak@gmail.com>
2020-12-11 09:47:18 +0900

    test: Areaの算出をメソッドに更新

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,7 +15,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
@@ -25,7 +25,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("circles", func(t *testing.T) {
 		circles := Circle{10}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 
 		if got != want {
