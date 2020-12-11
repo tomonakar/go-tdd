0ea7815c6666c659efc1520b8061d13f47cae429
tomonakar <tomohisa.nak@gmail.com>
2020-12-11 09:13:52 +0900

    テストコードと最小限のインターフェースを追加

---
+++ structs/shapes.go
@@ -0,0 +1,5 @@
+package structs
+
+func Perimeter(width, height float64) float64 {
+	return 0
+}
---
+++ structs/shapes_test.go
@@ -0,0 +1,12 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %2.f want %2.f", got, want)
+	}
+}
