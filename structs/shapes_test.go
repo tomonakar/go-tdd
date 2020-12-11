97d3e4f95ce33392a95a063c5098de298e465368
tomonakar <tomohisa.nak@gmail.com>
2020-12-11 09:46:12 +0900

    test: CircleのAreaを計算するテストを追加

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %2.f want %2.f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %2.f want %2.f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circles := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %2.f want %2.f", got, want)
+		}
+	})
 }
