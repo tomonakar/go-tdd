97fa39e2d0a46cfac414615ea74b41f77af8703c
tomonakar <tomohisa.nak@gmail.com>
2020-12-12 14:25:12 +0900

    test: テーブル駆動テストを追加

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -37,3 +37,22 @@ func TestArea(t *testing.T) {
 
 	})
 }
+
+// テーブル駆動テスト
+func TestArea2(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
