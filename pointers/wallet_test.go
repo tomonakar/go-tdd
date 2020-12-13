52a8766503db40d88f3fc874ae31c05bbb56cc4f
tomonakar <tomohisa.nak@gmail.com>
2020-12-13 13:23:33 +0900

    refactor: split test helper functions

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,23 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, err error) {
-		t.Helper()
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
@@ -39,8 +22,27 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, Bitcoin(20))
-		assertError(t, err)
-
+		assertError(t, err, ErrInsufficientFunds)
 	})
 
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got.Error() != want.Error() {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
