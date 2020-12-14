21b427b19f76c42bfb8e1c84a55417cdfc73951a
tomonakar <tomohisa.nak@gmail.com>
2020-12-14 16:18:38 +0900

    refactor: テスト用関数を分離

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -7,8 +7,14 @@ func TestSearch(t *testing.T) {
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertStrings(t, got, want)
+
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("got %q want %q given %q", got)
 	}
 }
