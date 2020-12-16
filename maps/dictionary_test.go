c0fb83f8976b5a3f8cca5098afb26329273f0c65
tomonakar <tomohisa.nak@gmail.com>
2020-12-17 08:10:24 +0900

    test: 存在しない単語ケースのテストを追加

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,11 +6,21 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-		got := dictionary.Search("test")
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 		assertStrings(t, got, want)
 	})
 
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
