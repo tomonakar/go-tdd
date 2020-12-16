454a3b3c1ce4c6a9b879517020994c68b1392754
tomonakar <tomohisa.nak@gmail.com>
2020-12-17 08:20:07 +0900

    feat: testを満たすコードを追加

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -36,7 +36,7 @@ func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
 	dictionary.Add("test", "this is just a test")
 
-	want := "test is just a test"
+	want := "this is just a test"
 	got, err := dictionary.Search("test")
 	if err != nil {
 		t.Fatal("should find added word: ", err)
