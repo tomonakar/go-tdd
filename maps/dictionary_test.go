5dadb481bd36ca145653d550beb7fef0ab9bc414
tomonakar <tomohisa.nak@gmail.com>
2020-12-17 09:08:37 +0900

    test: 追加しようとしている値が既に存在しているケースを追加

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -25,20 +25,40 @@ func assertStrings(t *testing.T, got, want string) {
 	}
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+}
+
 func assertError(t *testing.T, got, want error) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
-}
-
-func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
-
-	assertDefinition(t, dictionary, word, definition)
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
+	}
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
