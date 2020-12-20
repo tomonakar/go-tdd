2e9c595da2754cc519ceb32618aa17ea942a578a
tomonakar <tomohisa.nak@gmail.com>
2020-12-20 17:17:28 +0900

    sleeperをconfigurableに変更

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -69,11 +69,6 @@ func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 const write = "write"
 const sleep = "sleep"
 
-type ConfigurableSleeper struct {
-	duration time.Duration
-	sleep    func(time.Duration)
-}
-
 type SpyTime struct {
 	durationSlept time.Duration
 }
--- mocking/main.go
+++ mocking/main.go
@@ -14,11 +14,13 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct {
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -32,6 +34,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
