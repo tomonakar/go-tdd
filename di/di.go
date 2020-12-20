e2ceb2de3d17dda8aa1e878ea78b48c7530290bc
tomonakar <tomohisa.nak@gmail.com>
2020-12-20 10:53:11 +0900

    汎用化の参考例としてhttp.ResponseWriterでGreetを使用

--- di/di.go
+++ di/di.go
@@ -1,8 +1,10 @@
-package di
+package main
 
 import (
 	"fmt"
 	"io"
+	"net/http"
+	"os"
 )
 
 // この関数のテストは難しい
@@ -13,4 +15,13 @@ func GreetSample(name string) {
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "tomo")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
