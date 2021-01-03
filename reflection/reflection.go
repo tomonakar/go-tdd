38c787c10be6b8e2a90f9cd9f88f9441d6851265
tomonakar <tomohisa.nak@gmail.com>
2021-01-03 11:45:52 +0900

    refactor: swichケース化

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,21 +5,18 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
-		return
-	}
-
 	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
+		switch val.Kind() {
 		case reflect.Struct:
-			walk(field.Interface(), fn)
+			for i := 0; i < val.NumField(); i++ {
+				walk(val.Field(i).Interface(), fn)
+			}
+		case reflect.Slice:
+			for i := 0; i < val.Len(); i++ {
+				walk(val.Index(i).Interface(), fn)
+			}
+		case reflect.String:
+			fn(val.String())
 		}
 	}
 }
