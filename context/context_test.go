83b07578613cc74ebd31312d278e3cb3683510d7
tomonakar <tomohisa.nak@gmail.com>
2021-01-06 06:38:54 +0900

    test: responseWiterのテスト用構造体を作成し、エラー時に応答を書かないテストを作成

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package contextp
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -38,6 +39,24 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	}
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestHandler(t *testing.T) {
 	data := "Hello, world"
 
@@ -54,39 +73,26 @@ func TestHandler(t *testing.T) {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 		}
 
-		// store.assertWasNotCancelled()
 	})
 
-	// t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-	// 	store := &SpyStore{response: data, t: t}
-	// 	svr := Server(store)
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
 
-	// 	request := httptest.NewRequest(http.MethodGet, "/", nil)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
-	// 	cancellingCtx, cancel := context.WithCancel(request.Context())
-	// 	time.AfterFunc(5*time.Millisecond, cancel)
-	// 	request = request.WithContext(cancellingCtx)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
 
-	// 	response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
-	// 	svr.ServeHTTP(response, request)
+		svr.ServeHTTP(response, request)
 
-	// 	store.assertWasCancelled()
+		if response.written {
+			t.Error("a response should not have been written")
+		}
 
-	// })
+	})
 
 }
-
-// func (s *SpyStore) assertWasCancelled() {
-// 	s.t.Helper()
-// 	if !s.cancelled {
-// 		s.t.Errorf("store was not told to cancel")
-// 	}
-// }
-
-// func (s *SpyStore) assertWasNotCancelled() {
-// 	s.t.Helper()
-// 	if s.cancelled {
-// 		s.t.Errorf("store was told to cancel")
-// 	}
-// }
