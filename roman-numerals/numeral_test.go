d465366c0120630cd7ce88750407b34a6fd2929d
tomonakar <tomohisa.nak@gmail.com>
2021-01-07 05:51:22 +0900

    feat: added ConvertToArabic

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -54,7 +54,7 @@ func TestRomanNumerals(t *testing.T) {
 
 func TestConvertingToArabic(t *testing.T) {
 	for _, test := range cases[:1] {
-		t.Run(fmt.Sprint("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
 			got := ConvertToArabic(test.Roman)
 			if got != test.Arabic {
 				t.Errorf("got %d, want %d", got, test.Arabic)
