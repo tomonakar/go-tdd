cb5d0e06782f63b7ba2065a37eb56edba6ef9776
tomonakar <tomohisa.nak@gmail.com>
2021-01-10 10:35:10 +0900

    feat: SVGの時計を考慮した秒針作成

--- mathTest/clockface.go
+++ mathTest/clockface.go
@@ -10,8 +10,14 @@ type Point struct {
 	Y float64
 }
 
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(t)
+	p = Point{p.X * 90, p.Y * 90}   // scale: SVGの時計の長さに合わせる
+	p = Point{p.X, -p.Y}            // flip: 12時をスタート地点にするため、X軸上で反転させる
+	p = Point{p.X + 150, p.Y + 150} // translate: 正しい位置に移動
+	return p
 }
 
 func secondsInRadians(t time.Time) float64 {
