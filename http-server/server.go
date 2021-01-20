d32f5841aa0f520e7fbbcd636d8f266e69c2728b
tomonakar <tomohisa.nak@gmail.com>
2021-01-20 23:22:39 +0900

    test: added Floyd case

--- http-server/server.go
+++ http-server/server.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprintf(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprintf(w, "10")
+		return
+	}
 }
