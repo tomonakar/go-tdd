eeb3aa0bad802c35227e16fcb2eca94316238c1f
tomonakar <tomohisa.nak@gmail.com>
2021-01-24 16:49:59 +0900

    feat: Post時にwinを記録するテストコードを通すためのコードを追加

--- http-server/server.go
+++ http-server/server.go
@@ -8,6 +8,7 @@ import (
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 type PlayerServer struct {
@@ -34,6 +35,7 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	p.store.RecordWin("Bob")
 	w.WriteHeader(http.StatusAccepted)
 }
 
